e989360fe4ff133bc9bdae570530832bdcebf7a5
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-03-10 12:54:51 +0200

    added ˇmenu to other page

--- app_b.php
+++ app_b.php
@@ -15,15 +15,24 @@
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		  
-		  
+		
 		  <ul class="nav navbar-nav">
 			
-			<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-
-		  </ul>
-		
-		
+			<li class="active">
+				<a href="app_b.php">
+					App page
+				</a>
+			</li>
+			
+			
+			<li>
+				<a href="table_b.php">
+					Table
+				</a>
+			</li>
+			
+		  </ul> 
+		  
 		</div><!-- /.navbar-collapse -->
 	  </div><!-- /.container-fluid -->
 	</nav>
