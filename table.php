bb49d24f5f98057e9703644a6c8882e00a4575f7
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-03-10 11:17:46 +0200

    added deleting

--- table.php
+++ table.php
@@ -7,8 +7,42 @@
 	//create connection
 	$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_romil");
 	
+	
+	/*
+		IF THERE IS ?DELETE=ROW_ID in the url
+	*/
+	if(isset($_GET["delete"])){
+		
+		echo "Deleting row with id:".$_GET["delete"];
+		
+		// NOW() = current date-time
+		$stmt = $mysql->prepare("UPDATE messages_sample SET deleted=NOW() WHERE id = ?");
+		
+		echo $mysql->error;
+		
+		//replace the ?
+		$stmt->bind_param("i", $_GET["delete"]);
+		
+		if($stmt->execute()){
+			echo "deleted successfully";
+		}else{
+			echo $stmt->error;
+		}
+		
+		//closes the statement, so others can use connection
+		$stmt->close();
+		
+	}
+	
+	
+	
+	
+	
+	
 	//SQL sentence
-	$stmt = $mysql->prepare("SELECT id, recipient, message, created FROM messages_sample ORDER BY created DESC LIMIT 10");
+	$stmt = $mysql->prepare("SELECT id, recipient, message, created FROM messages_sample WHERE deleted IS NULL ORDER BY created DESC LIMIT 10");
+	
+	//WHERE deleted IS NULL show only those that are not deleted
 	
 	//if error in sentence
 	echo $mysql->error;
@@ -28,6 +62,7 @@
 			$table_html .= "<th>Recipient</th>";
 			$table_html .= "<th>Message</th>";
 			$table_html .= "<th>Created</th>";
+			$table_html .= "<th>Delete ?</th>";
 		$table_html .= "</tr>";
 	
 	// GET RESULT 
@@ -41,6 +76,10 @@
 			$table_html .= "<td>".$recipient."</td>";
 			$table_html .= "<td>".$message."</td>";
 			$table_html .= "<td>".$created."</td>";
+			$table_html .= "<td>
+								<a href='?delete=".$id."'>X</a>
+							</td>";
+			
 		$table_html .= "</tr>"; //end row
 
 	}
